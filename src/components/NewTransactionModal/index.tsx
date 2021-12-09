5f20a3950003830683e05b2e035404a5a127d97a
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-09 18:22:21 -0300

    feat: changes modal income and outcome buttons colors

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,15 +2,18 @@ import Modal from 'react-modal'
 import incomeImg from '../../assets/Entradas.svg'
 import outcomeImg from '../../assets/Saidas.svg'
 import closeImg from '../../assets/Botao-Fechar.svg'
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
-
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
+
   return (
     <Modal 
         isOpen={isOpen} 
@@ -40,19 +43,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           />
 
           <TransactionTypeContainer>
-            <button
+            <RadioBox
               type="button"
+              onClick={() => { setType('deposit'); }}
+              isActive={type === 'deposit'}
+              activeColor="green"
             >
               <img src={incomeImg} alt="Entrada" />
               <span>Entrada</span>
-            </button>
+            </RadioBox>
 
-            <button
+            <RadioBox
               type="button"
+              onClick={() => { setType('withdraw');}}
+              isActive={type === 'withdraw'}
+              activeColor="red"
             >
             <img src={outcomeImg} alt="Saída" />
               <span>Saída</span>
-            </button>
+            </RadioBox>
           </TransactionTypeContainer>
 
           <input 
