141230c2d2c3ebf472463e664c31ca9743779472
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-13 14:38:01 -0300

    feat: creates useTransaction hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,13 @@
-import { useContext } from 'react';
+import { useTransactions } from '../../hooks/useTransactions';
+
 import incomeImg from '../../assets/Entradas.svg'
 import outcomeImg from '../../assets/Saidas.svg'
 import totalImg from '../../assets/Total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   // const totalDeposits = transactions.reduce((acc, transaction) => {
   //   if (transaction.type === 'deposit') {
