908318097a2e8c3d2ad7af3a73acc7458030e20f
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-13 14:25:18 -0300

    feat: calculate transactions resume

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ createServer({
           title: 'Freelance de website',
           type: 'deposit',
           category: 'Dev',
-          amount: '6000',
+          amount: 6000,
           createdAt: new Date('2021-02-12 09:00:00')
         },
         {
@@ -24,7 +24,7 @@ createServer({
           title: 'Aluguel',
           type: 'withdraw',
           category: 'Casa',
-          amount: '1100',
+          amount: 1100,
           createdAt: new Date('2021-02-14 11:00:00')
         }
       ]
