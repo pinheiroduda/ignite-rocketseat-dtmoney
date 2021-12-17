1dae3db68cbbe75f4a178cec64ee5dcd1e0fcaec
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-17 15:03:22 -0300

    feat: change summary total background color

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -62,7 +62,7 @@ export function Summary() {
         </strong>
       </div>
 
-      <div>
+      <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
